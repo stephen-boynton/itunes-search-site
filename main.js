c8863d6a8497d8d6d15136b0e1bddff7123e01eb
Stephen Boynton <boyntonsc@gmail.com>
2017-08-11 11:19:13 -0400

    Finish project

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
-
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
-
-// DON'T FORGET TO ENCODE TO URL LANG
-//=========================================================================
-/*
-===============data names==========================
-- artworkUrl100 - thumbnail
--trackName - track name
-
-===============Seach types==========================
-- https://itunes.apple.com/search?term= - the beginning key
-- term - search term
--&media=music - limited to music
--&limit=20 - limited to 20 results
--&explicit=yes or no - give the option
-
-*/
 
 //============= Globals ================================
 let searchTerm = "";
@@ -34,7 +9,7 @@ const urlBase = `https://itunes.apple.com/search?term=`;
 const urlEnd = `&media=music&limit=20`
 form.addEventListener("submit", appRun);
 let destroy = false;
-const previewURL = [];
+let previewURL = [];
 let audioArray = [];
 
 function returnURL (str) {
@@ -65,7 +40,6 @@ function buildSong(song, counter) {
 
 function buildSongs(library) {
   let songs = library.results;
-  console.log(songs);
   let counter = 0;
   for (let i = 0; i < songs.length; i++) {
     buildSong(songs[i], counter);
@@ -79,13 +53,11 @@ function populatePreviews () {
   for (let i = 0; i < audioArray.length; i++) {
     audioArray[i] = ['p' + i.toString(), previewURL[i]]
    }
-   console.log(audioArray);
 }
 
 function playSong (event) {
   const audio = document.querySelector("audio")
   let targetSong = event.target.id;
-  console.log(targetSong);
   for (let i = 0; i < audioArray.length; i++) {
     if (audioArray[i][0] == targetSong) audio.setAttribute("src", audioArray[i][1]);
   }
@@ -97,6 +69,8 @@ function destroySite() {
 		let song = document.querySelector(".song");
 		results.removeChild(song);
 	}
+  previewURL = [];
+  audioArray = [];
 }
 
 function appRun (event) {
@@ -104,7 +78,6 @@ function appRun (event) {
   if(destroy === true) destroySite();
   searchTerm = event.target[0].value;
   returnURL(searchTerm);
-  console.log(url);
   getSongs(url);
   event.target[0].value = ""
   destroy = true;
